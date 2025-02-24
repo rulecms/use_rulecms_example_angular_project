74f27f1f9ba8f5fcc87739d786437a269fb9591a
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-24 18:58:35 -0500

    use rulecms client package for operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { add, subtract } from '@rulecms/client';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'use_rulecms_example_angular_project';
+  addResult = 0;
+  subtractResult = 0;
+  ngOnInit() {
+    this.addResult = add(1, 2);
+    this.subtractResult = subtract(1, 2);
+  }
 }
