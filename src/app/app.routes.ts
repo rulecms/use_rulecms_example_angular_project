7fd458afafad88e39b3ce2e76f365e4c620913fd
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-25 13:13:18 -0500

    lazy load config viewer module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'config-viewer',
+    loadChildren: () => import('./config-viewer/config-viewer.module').then(m => m.ConfigViewerModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
