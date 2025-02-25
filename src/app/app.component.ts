028190c21f88dabe837dc1b7ad07eda9510f84b1
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-25 13:12:56 -0500

    added router link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { add, subtract } from '@rulecms/client';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
